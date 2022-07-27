4314f49850965252b2aaaffd85621abe2615ae69
Rakesh Kumar Sahoo <34296104+webkitty10@users.noreply.github.com>
2022-07-27 16:21:58 +0530

    Add files via upload

---
+++ src/App.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+
+import reducers from "../src/reducers";
+
+const store = createStore(reducers, applyMiddleware(reduxThunk));
+
+import App from "./App";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
+});
